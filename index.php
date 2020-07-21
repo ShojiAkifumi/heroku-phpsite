d3c6bd2f67806e40e080c8a4a6c449b673f2fa6d
ShojiAkifumi <kuonnrh48951@outlook.jp>
2020-07-21 23:25:40 +0900

    add header phpfile

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
-
-header('Location: /index.html');
-
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php
+            header('Location: /header.html');
+        ?>
+    </header>
+    <h1>
+        <?php
+            echo "Hello, Heroku!";
+        ?>
+    </h1>
+    <p>こんにちは</p>
+</body>
+</html>
